aaf3bf13634bbf308cedec0a0ec3ba716e465ae6
Alan Eduardo Ramirez Salazar <chusterrs87@gmail.com>
2024-10-26 10:31:19 -0600

    apiFlask

---
+++ src/config.py
@@ -0,0 +1,13 @@
+class DevelopmentConfig():
+    DEBUG = True
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER= 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB= 'api_utl'
+    ssl_disable=True
+   
+config ={
+    'development': DevelopmentConfig
+}
+   
+   
